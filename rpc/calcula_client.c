0350f5c78bbfcb134643554d86f25552394257e4
Kleidson Alves <kleidsonalves15@gmail.com>
2022-06-20 18:13:44 -0300

    Entrega 2: 2 Workers

--- rpc/calcula_client.c
+++ rpc/calcula_client.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <math.h>
 
-
-
 #define X 500000
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 int f_aleat() {
     int aleat = rand() % X;
@@ -18,31 +19,46 @@ void generateNumbers(float *v) {
 }
 
 int main(int argc, char* argv[]) {
-	CLIENT *clnt;
+	CLIENT *clnt, *clnt2;
     Data data;
-    Result msg;
+    Result msg, msg2;
    
     srand(time(NULL));
 
-	if (argc!=2) {
-		fprintf(stderr,"Uso: %s hostname\n",argv[0]);
+	if (argc!=3) {
+		fprintf(stderr,"Uso: %s hostname1 hostname2\n",argv[0]);
 	exit(0); 
 	}
 
 	clnt = clnt_create(argv[1], PROG, VERSAO, "tcp");
+	clnt2 = clnt_create(argv[2], PROG, VERSAO, "tcp");
 
 	if (clnt == (CLIENT *) NULL) {
 		clnt_pcreateerror(argv[1]);
+		exit(1);
+	}
+
+    if (clnt2 == (CLIENT *) NULL) {
+		clnt_pcreateerror(argv[2]);
 		exit(1);
 	}
     
     generateNumbers(data.v);
 
     data.start = 0;
-    data.end = X - 1;
+    data.end = X/2 - 1;
 
 	msg = *findnumbers_100(&data, clnt);
+    
+    data.start = X/2;
+    data.end = X - 1;
 
-    printf("min = %.2f\nmax = %.2f\n", msg.min, msg.max);
+	msg2 = *findnumbers_100(&data, clnt2);
+
+    float min = MIN(msg.min, msg2.min);
+    float max = MAX(msg.max, msg2.max);
+
+    printf("min = %.2f\nmax = %.2f\n", min, max);
+
     return 0;
 }
